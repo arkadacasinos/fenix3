4091e9ddb567a29a6d65167988f4ff0e9c12d9e5
arkadacasinos <bezdep.rf@mail.ru>
2026-05-20 04:16:46 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({
   variable: "--font-inter",
 })
 
-const title = `${siteConfig.name} официальный сайт онлайн казино Феникс зеркало `
+const title = `${siteConfig.name} официальный сайт онлайн казино Феникс `
 const titleTemplate = `%s | ${siteConfig.name}`
 
 export const metadata: Metadata = {
